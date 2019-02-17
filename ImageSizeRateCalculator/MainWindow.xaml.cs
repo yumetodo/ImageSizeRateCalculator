eb83036ea629aeb88efe68a721540378d4054c1a
yumetodo <yume-wikijp@live.jp>
2019-02-17 19:36:31 +0900

    プロジェクト ファイルを追加します。

---
+++ ImageSizeRateCalculator/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageSizeRateCalculator
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ResultPreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+        private async void ResultDropFiles(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if(2 == files?.Length)
+            {
+                try
+                {
+                    var sizes = await Task.WhenAll(files.Select(async fileName => await Task.Run(() => System.Drawing.Image.FromFile(fileName).Size)));
+                    result.Text
+                        = $"縦: x{Math.Round(sizes[0].Height * 1.0 / sizes[1].Height, 4)} x{Math.Round(sizes[1].Height * 1.0 / sizes[0].Height, 4)}\n"
+                        + $"横: x{Math.Round(sizes[0].Width * 1.0 / sizes[1].Width, 4)} x{Math.Round(sizes[1].Width * 1.0 / sizes[0].Width, 4)}";
+                }
+                catch (OutOfMemoryException)
+                {
+                    result.Text = "入力は本当に画像ですか？";
+                }
+                catch (Exception er)
+                {
+                    result.Text = "error\n" + er.ToString();
+                }
+            }
+            else
+            {
+                result.Text = "";
+            }
+        }
+        private void OnCtrlC(object sender, RoutedEventArgs e)
+        {
+            if (string.Empty != result.Text)
+            {
+                Clipboard.SetData(DataFormats.Text, result.Text);
+            }
+        }
+    }
+}
